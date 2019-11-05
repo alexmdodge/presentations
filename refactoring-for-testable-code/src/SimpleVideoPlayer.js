848e882f178e5e0c852a6ce1b1ac3db38392ab57
Alex Dodge <alex.dodge@redspace.com>
2019-11-05 10:04:02 +0100

    step-7: move the specific play paths - Move the specific play paths into their own methods - Note that we're getting to the point where we're noticing methods that do multiple things that don't necessarily need to happen

--- refactoring-for-testable-code/src/SimpleVideoPlayer.js
+++ refactoring-for-testable-code/src/SimpleVideoPlayer.js
@@ -62,111 +62,9 @@ class SimpleVideoPlayer {
     const isHlsVideoPlaybackSupported = this._detectHlsVideoPlaybackSupport(videoType)
 
     if (isNativeVideoPlaybackSupported) {
-      // Load the basic video player as expected
-      this._videoElement.src = videoUrl
-      this._videoElement.addEventListener('loadedmetadata', () => {
-        // Once things start playing then we'll create our controls and inject them
-        // in as well
-        this._videoContainerElement.style.position = 'relative'
-        this._uiContainerElement = document.createElement('div')
-        this._uiContainerElement.style.display = 'flex'
-        this._uiContainerElement.style.flexDirection = 'row'
-        this._uiContainerElement.style.justifyContent = 'space-between'
-        this._uiContainerElement.style.alignItems = 'bottom'
-        this._uiContainerElement.style.position = 'absolute'
-        this._uiContainerElement.style.width = '80%'
-        this._uiContainerElement.style.height = '100%'
-        this._uiContainerElement.style.margin = '10px'
-        this._uiContainerElement.style.top = '0'
-
-        // Then make the play button with all of its styles
-        const play = document.createElement('button')
-        play.innerHTML = 'PLAY'
-        play.style.fontFamily = `"Comic Sans MS", "Comic Sans", cursive`
-        play.style.fontSize = '16px'
-        play.style.fontWeight = 'bold'
-        play.style.height = '60px'
-
-        play.addEventListener('click', () => {
-          this._videoElement.play()
-        })
-
-        // And pause
-        const pause = document.createElement('button')
-        pause.innerHTML = 'PAUSE'
-        pause.style.fontFamily = `"Comic Sans MS", "Comic Sans", cursive`
-        pause.style.fontSize = '16px'
-        pause.style.fontWeight = 'bold'
-        pause.style.height = '60px'
-
-        pause.addEventListener('click', () => {
-          this._videoElement.pause()
-        })
-
-        this._ui.appendChild(play)
-        this._ui.appendChild(pause)
-        this._videoContainerElement.appendChild(this._ui)
-
-        // Then start the video
-        this._videoElement.play();
-      });
+      this._setupUiAndPlayNativeVideo(videoUrl)
     } else if (isHlsVideoPlaybackSupported) {
-        this._hls = new Hls();
-
-        // Load the source into the hls instance
-        this._hls.loadSource(videoUrl);
-
-        // Attach the video
-        this._hls.attachMedia(this._videoElement);
-
-        // Finally listen for when the manifest is parsed
-        // and we can start to play
-        this._hls.on(Hls.Events.MANIFEST_PARSED, () => {
-          console.log('Manifest is parsed')
-          // Setup the same UI for the
-          this._videoContainerElement.style.position = 'relative'
-          this._ui = document.createElement('div')
-          this._ui.style.display = 'flex'
-          this._ui.style.flexDirection = 'row'
-          this._ui.style.justifyContent = 'space-between'
-          this._ui.style.alignItems = 'center'
-          this._ui.style.position = 'absolute'
-          this._ui.style.width = '80%'
-          this._ui.style.height = '100%'
-          this._ui.style.top = '0'
-          this._ui.style.margin = '10px'
-
-          // Then make the play button with all of its styles
-          const play = document.createElement('button')
-          play.innerHTML = 'PLAY'
-          play.style.fontFamily = `"Comic Sans MS", "Comic Sans", cursive`
-          play.style.fontSize = '16px'
-          play.style.fontWeight = 'bold'
-          play.style.height = '60px'
-
-          // And pause
-          const pause = document.createElement('button')
-          pause.innerHTML = 'PAUSE'
-          pause.style.fontFamily = `"Comic Sans MS", "Comic Sans", cursive`
-          pause.style.fontSize = '16px'
-          pause.style.fontWeight = 'bold'
-          pause.style.height = '60px'
-
-          play.addEventListener('click', () => {
-            this._videoElement.play()
-          })
-
-          pause.addEventListener('click', () => {
-            this._videoElement.pause()
-          })
-
-          this._ui.appendChild(play)
-          this._ui.appendChild(pause)
-          this._videoContainerElement.appendChild(this._ui)
-
-          // Then play
-          this._videoElement.play()
-        })
+      this._setupUiAndPlayHlsVideo(videoUrl)
     } else {
       console.error('url type is not supported')
     }
@@ -186,6 +84,116 @@ class SimpleVideoPlayer {
     const isVideoTypeHls = videoType === 'm3u8'
     const isHlsSupportAvailable = Hls.isSupported()  
     return isVideoTypeHls && isHlsSupportAvailable
+  }
+
+  _setupUiAndPlayNativeVideo(videoUrl) {
+    // Load the basic video player as expected
+    this._videoElement.src = videoUrl
+    this._videoElement.addEventListener('loadedmetadata', () => {
+      // Once things start playing then we'll create our controls and inject them
+      // in as well
+      this._videoContainerElement.style.position = 'relative'
+      this._uiContainerElement = document.createElement('div')
+      this._uiContainerElement.style.display = 'flex'
+      this._uiContainerElement.style.flexDirection = 'row'
+      this._uiContainerElement.style.justifyContent = 'space-between'
+      this._uiContainerElement.style.alignItems = 'bottom'
+      this._uiContainerElement.style.position = 'absolute'
+      this._uiContainerElement.style.width = '80%'
+      this._uiContainerElement.style.height = '100%'
+      this._uiContainerElement.style.margin = '10px'
+      this._uiContainerElement.style.top = '0'
+
+      // Then make the play button with all of its styles
+      const play = document.createElement('button')
+      play.innerHTML = 'PLAY'
+      play.style.fontFamily = `"Comic Sans MS", "Comic Sans", cursive`
+      play.style.fontSize = '16px'
+      play.style.fontWeight = 'bold'
+      play.style.height = '60px'
+
+      play.addEventListener('click', () => {
+        this._videoElement.play()
+      })
+
+      // And pause
+      const pause = document.createElement('button')
+      pause.innerHTML = 'PAUSE'
+      pause.style.fontFamily = `"Comic Sans MS", "Comic Sans", cursive`
+      pause.style.fontSize = '16px'
+      pause.style.fontWeight = 'bold'
+      pause.style.height = '60px'
+
+      pause.addEventListener('click', () => {
+        this._videoElement.pause()
+      })
+
+      this._ui.appendChild(play)
+      this._ui.appendChild(pause)
+      this._videoContainerElement.appendChild(this._ui)
+
+      // Then start the video
+      this._videoElement.play();
+    });
+  }
+
+  _setupUiAndPlayHlsVideo(videoUrl) {
+    this._hls = new Hls();
+
+    // Load the source into the hls instance
+    this._hls.loadSource(videoUrl);
+
+    // Attach the video
+    this._hls.attachMedia(this._videoElement);
+
+    // Finally listen for when the manifest is parsed
+    // and we can start to play
+    this._hls.on(Hls.Events.MANIFEST_PARSED, () => {
+      console.log('Manifest is parsed')
+      // Setup the same UI for the
+      this._videoContainerElement.style.position = 'relative'
+      this._ui = document.createElement('div')
+      this._ui.style.display = 'flex'
+      this._ui.style.flexDirection = 'row'
+      this._ui.style.justifyContent = 'space-between'
+      this._ui.style.alignItems = 'center'
+      this._ui.style.position = 'absolute'
+      this._ui.style.width = '80%'
+      this._ui.style.height = '100%'
+      this._ui.style.top = '0'
+      this._ui.style.margin = '10px'
+
+      // Then make the play button with all of its styles
+      const play = document.createElement('button')
+      play.innerHTML = 'PLAY'
+      play.style.fontFamily = `"Comic Sans MS", "Comic Sans", cursive`
+      play.style.fontSize = '16px'
+      play.style.fontWeight = 'bold'
+      play.style.height = '60px'
+
+      // And pause
+      const pause = document.createElement('button')
+      pause.innerHTML = 'PAUSE'
+      pause.style.fontFamily = `"Comic Sans MS", "Comic Sans", cursive`
+      pause.style.fontSize = '16px'
+      pause.style.fontWeight = 'bold'
+      pause.style.height = '60px'
+
+      play.addEventListener('click', () => {
+        this._videoElement.play()
+      })
+
+      pause.addEventListener('click', () => {
+        this._videoElement.pause()
+      })
+
+      this._ui.appendChild(play)
+      this._ui.appendChild(pause)
+      this._videoContainerElement.appendChild(this._ui)
+
+      // Then play
+      this._videoElement.play()
+    })
   }
 
   play() {
