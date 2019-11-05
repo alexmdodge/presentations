e59dc35a97cdefa400cfe5910132775d3ce35a79
Alex Dodge <alex.dodge@redspace.com>
2019-11-05 13:29:38 +0100

    step-13: co-locate simple things - In some cases additional methods just add bloat and if something is clear we don't have to keep it standalone - Also organize our public and private to keep an idea of what our public contract is

--- refactoring-for-testable-code/src/SimpleVideoPlayer.js
+++ refactoring-for-testable-code/src/SimpleVideoPlayer.js
@@ -24,6 +24,8 @@ class SimpleVideoPlayer {
     this._attachVideoElementToContainer()
   }
 
+  // PRIVATE METHODS
+
   _applyPlayerOptionsToVideoElement() {
     if (this._playerOptions.autoPlay === true) {
       applyAutoPlayAttributesToVideoElement(this._videoElement)
@@ -38,6 +40,39 @@ class SimpleVideoPlayer {
     this._videoContainerElement.appendChild(this._videoElement)
   }
 
+  _detectVideoFormatAndLoad(videoUrl) {
+    const videoType = detectVideoTypeFromUrl(videoUrl)
+    const isNativeVideoPlaybackSupported = detectNativeVideoPlaybackSupport(videoType, this._videoElement)
+    const isHlsVideoPlaybackSupported = detectHlsVideoPlaybackSupport(videoType, Hls)
+
+    if (isNativeVideoPlaybackSupported) {
+      this._playNativeVideo(videoUrl)
+    } else if (isHlsVideoPlaybackSupported) {
+      this._playHlsVideo(videoUrl)
+    } else {
+      console.error('Video URL type is not supported')
+    }
+  }
+
+  _playNativeVideo(videoUrl) {
+    this._videoElement.src = videoUrl
+    this._videoElement.addEventListener('loadedmetadata', () => {
+      this._videoElement.play()
+    });
+  }
+
+  _playHlsVideo(videoUrl) {
+    this._hls = new Hls();
+    this._hls.loadSource(videoUrl);
+    this._hls.attachMedia(this._videoElement);
+
+    this._hls.on(Hls.Events.MANIFEST_PARSED, () => {
+      this._videoElement.play()
+    })
+  }
+
+  // PUBLIC API METHODS
+
   load(videoUrl) {
     const isVideoUrlValidFormat = validateVideoUrlValidFormat(videoUrl)
 
@@ -46,45 +81,6 @@ class SimpleVideoPlayer {
     } else {
       console.error('Video URL is not a valid format')
     }
-  }
-
-  _detectVideoFormatAndLoad(videoUrl) {
-    const videoType = detectVideoTypeFromUrl(videoUrl)
-    const isNativeVideoPlaybackSupported = detectNativeVideoPlaybackSupport(videoType, this._videoElement)
-    const isHlsVideoPlaybackSupported = detectHlsVideoPlaybackSupport(videoType, Hls)
-
-    if (isNativeVideoPlaybackSupported) {
-      this._setupUiAndPlayNativeVideo(videoUrl)
-    } else if (isHlsVideoPlaybackSupported) {
-      this._setupUiAndPlayHlsVideo(videoUrl)
-    } else {
-      console.error('Video URL type is not supported')
-    }
-  }
-
-  _setupUiAndPlayNativeVideo(videoUrl) {
-    this._videoElement.src = videoUrl
-    this._videoElement.addEventListener('loadedmetadata', () => {
-      // Note we could bind here or define a method using the Babel class
-      // properties transform
-      this._onNativeVideoMetadataLoaded()
-    });
-  }
-
-  _onNativeVideoMetadataLoaded() {
-    this._setupVideoPlayerUi()
-    this._videoElement.play();
-  }
-
-  _setupUiAndPlayHlsVideo(videoUrl) {
-    this._hls = new Hls();
-    this._hls.loadSource(videoUrl);
-    this._hls.attachMedia(this._videoElement);
-
-    this._hls.on(Hls.Events.MANIFEST_PARSED, () => {
-      this._setupVideoPlayerUi()
-      this._videoElement.play()
-    })
   }
 
   play() {
