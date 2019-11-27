da09895a8b318509c9053d32c8f0f8c7a3c65045
Alex Dodge <alex.dodge@redspace.com>
2019-11-27 03:31:25 +0100

    task: update partial types demo

--- typescript-partial-types/deep-partial.ts
+++ typescript-partial-types/deep-partial.ts
@@ -1,15 +1,51 @@
-// Expanded from the original:
+
+
+// In Progress
+type NonRecursive =
+ | string
+ | number
+ | boolean
+ | undefined
+ | null
+ | Date
+ | Function
+
+export type DeepPartial<T> = 
+  T extends NonRecursive ? T :
+  T extends {} ? {
+    [K in keyof T]?: DeepPartial<T[K]>
+  } :
+  Partial<T>
+
+
+// Original:
 // https://github.com/krzkaczor/ts-essentials/blob/master/lib/types.ts#L9
 
-export type DeepPartial<T> = {
-  [P in keyof T]?: 
-  T[P] extends string ? string :
-  T[P] extends boolean ? boolean :
-  T[P] extends number ? number :
-  T[P] extends null ? null :
-  T[P] extends Function ? Function :
-  T[P] extends Date ? Date :
-  T[P] extends Array<infer U> ? Array<DeepPartial<U>> :
-  T[P] extends ReadonlyArray<infer U> ? ReadonlyArray<DeepPartial<U>> :
-    DeepPartial<T[P]>
-};
+/** Essentials */
+export type Primitive = string | number | boolean | bigint | symbol | undefined | null;
+
+/** Dictionaries related */
+export type Dictionary<T, K extends string | number = string> = { [key in K]: T };
+export type DictionaryValues<T> = T extends Dictionary<infer U> ? U : never;
+
+/** Like Partial but recursive */
+export type OtherDeepPartial<T> = T extends Primitive | Function | Date
+  ? T
+  : T extends Map<infer K, infer V>
+  ? DeepPartialMap<K, V>
+  : T extends Set<infer U>
+  ? DeepPartialSet<U>
+  : T extends {}
+  ? { [K in keyof T]?: OtherDeepPartial<T[K]> }
+  : Partial<T>;
+interface DeepPartialSet<ItemType> extends Set<OtherDeepPartial<ItemType>> {}
+interface DeepPartialMap<KeyType, ValueType> extends Map<OtherDeepPartial<KeyType>, OtherDeepPartial<ValueType>> {}
+
+// Array index type query sample
+type Book = {
+  title: string;
+}
+
+type ArrayOfBooks = Book[]
+type MyArray = keyof ArrayOfBooks
+type MyArrayValue = ArrayOfBooks[MyArray]
---
+++ typescript-partial-types/video-player/index.ts
@@ -0,0 +1,87 @@
+import { DeepPartial } from '../deep-partial';
+import extend from 'extend'
+
+// To start, a large player configuration 
+type PlayerConfiguration = {
+  media: {
+    volume: number;
+    muted: boolean;
+    bitrate: number;
+  },
+  captions: {
+    enabled: boolean,
+    styles: {
+      color: 'red',
+      background: 'white',
+      font: 'italic'
+    }
+  },
+  ui: {
+    controls: { name: string, position: number }[],
+    themeColor: string;
+  }
+}
+
+// And the default configuration which is used for every player setup
+const playerConfigDefaults: PlayerConfiguration = {
+  media: {
+    volume: 1,
+    muted: false,
+    bitrate: 2000,
+  },
+  captions: {
+    enabled: true,
+    styles: {
+      color: 'red',
+      background: 'white',
+      font: 'italic'
+    }
+  },
+  ui: {
+    controls: [
+      { name: 'Play', position: 0 },
+      { name: 'Pause', position: 1 },
+      { name: 'Volume', position: -1 },
+    ],
+    themeColor: 'blue'
+  }
+}
+
+class VideoPlayer {
+  private _config: PlayerConfiguration
+
+  public constructor(playerConfig: DeepPartial<PlayerConfiguration>) {
+    this._config = this._normalizePlayerConfiguration(playerConfig)
+  }
+
+  private _normalizePlayerConfiguration(partialConfig: DeepPartial<PlayerConfiguration>): PlayerConfiguration {
+    // If we wanted we could add some runtime validation in here:
+    // https://github.com/gcanti/io-ts
+
+    return extend(true, {}, playerConfigDefaults, partialConfig)
+  }
+
+  public play(url: string, playOverrides?: DeepPartial<PlayerConfiguration>) {
+    this._config = this._overrideConfiguration(playOverrides)
+
+    // Play the video using our underlying video libraries
+    // _videoElement.volume = this._config.media.volume
+    // ex. _videoElement.src = url
+  }
+
+  private _overrideConfiguration(partialConfig?: DeepPartial<PlayerConfiguration>): PlayerConfiguration {
+    return extend(true, {}, this._config, partialConfig)
+  }
+}
+
+const player1 = new VideoPlayer({
+  captions: {
+    enabled: false
+  }
+})
+
+player1.play('https://my.video.url', {
+  media: {
+    volume: 0.5
+  }
+})
