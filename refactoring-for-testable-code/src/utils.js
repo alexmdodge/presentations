ef23c2835c96e2999d88b4b6aa9a6c00e7d1301f
Alex Dodge <alex.dodge@redspace.com>
2020-05-19 12:27:58 +0200

    chore: Categorize function types

--- refactoring-for-testable-code/src/utils.js
+++ refactoring-for-testable-code/src/utils.js
@@ -1,3 +1,7 @@
+/**
+ * Message: Incoming
+ * Type: Query
+ */
 export function createVideoPlayerElement() {
   const videoElement = document.createElement('video')
   videoElement.style.width = '100%'
@@ -6,6 +10,10 @@ export function createVideoPlayerElement() {
   return videoElement
 }
 
+/**
+ * Message: Incoming
+ * Type: Command
+ */
 export function applyAutoPlayAttributesToVideoElement(videoElement) {
   videoElement.setAttribute('autoplay', 'true')
 
@@ -13,25 +21,45 @@ export function applyAutoPlayAttributesToVideoElement(videoElement) {
   videoElement.muted = true
 }
 
+/**
+ * Message: Incoming
+ * Type: Command
+ */
 export function applyControlAttributesToVideoElement(videoElement) {
   videoElement.removeAttribute('controls', 'true')
 }
 
+/**
+ * Message: Incoming
+ * Type: Query
+ */
 export function validateVideoUrlValidFormat(url) {
   // We can now easily add more layers of validation here
   return url.lastIndexOf('.') > -1
 }
 
+/**
+ * Message: Incoming
+ * Type: Query
+ */
 export function detectVideoTypeFromUrl(url) {
   return url.substring(url.lastIndexOf('.') + 1)
 }
 
+/**
+ * Message: Incoming
+ * Type: Query
+ */
 export function detectNativeVideoPlaybackSupport(videoType, videoElement) {
   const isHlsSupportedNatively = videoElement.canPlayType('application/vnd.apple.mpegurl')
   const isVideoTypeProgressive = videoType === 'mp4'
   return isVideoTypeProgressive || isHlsSupportedNatively 
 }
 
+/**
+ * Message: Incoming
+ * Type: Query
+ */
 export function detectHlsVideoPlaybackSupport(videoType, HlsUtils) {
   const isVideoTypeHls = videoType === 'm3u8'
   const isHlsSupportAvailable = HlsUtils.isSupported()  
