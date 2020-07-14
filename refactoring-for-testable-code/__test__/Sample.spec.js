68f01945577bda9f9041bb342941171b68247d62
Alex Dodge <alex.dodge@redspace.com>
2020-07-14 13:53:41 +0200

    chore: RFTC Test part 3

--- refactoring-for-testable-code/__test__/Sample.spec.js
+++
@@ -1,5 +0,0 @@
-describe('sample', () => {
-  it('Given 1 is added to 1, we should get 2', () => {
-    expect(1 + 1).toStrictEqual(2)
-  })
-})
--- refactoring-for-testable-code/__test__/utils.spec.js
+++ refactoring-for-testable-code/__test__/utils.spec.js
@@ -1,4 +1,7 @@
-import { validateVideoUrlValidFormat } from '../src/utils'
+import {
+  validateVideoUrlValidFormat,
+  detectVideoTypeFromUrl
+} from '../src/utils'
 
 describe('validateVideoUrlValidFormat', () => {
   it('should return true if a valid format is detected', () => {
@@ -10,10 +13,38 @@ describe('validateVideoUrlValidFormat', () => {
   })
 
   it('should return false if an invalid format is detected', () => {
-    const testUrl = 'my-not-video'
+    const testUrl = 'not-a-video'
     
     expect(
       validateVideoUrlValidFormat(testUrl)
     ).toStrictEqual(false)
   })
+
+  // TDD for some additional cases
+  // it('should return false if url is falsy', () => {
+  //   const testUrl = null
+  //  
+  //   expect(
+  //     validateVideoUrlValidFormat(testUrl)
+  //   ).toStrictEqual(false)
+  // })
+})
+
+describe('detectVideoTypeFromUrl', () => {
+  it('should return mp4 for an mp4 video', () => {
+    const testUrl = 'my-video.mp4'
+
+    expect(
+      detectVideoTypeFromUrl(testUrl)
+    ).toStrictEqual('mp4')
+  })
+
+  // TDD for some additional cases
+  // it('should return null value if not extension found', () => {
+  //   const testUrl = 'not-a-video'
+    
+  //   expect(
+  //     detectVideoTypeFromUrl(testUrl)
+  //   ).toStrictEqual('')
+  // })
 })
